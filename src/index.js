a71fec89f2d261b2784e71b689859ecdd552991a
ElenaYa <elena.yatsynyuk@gmail.com>
2024-04-18 21:06:42 +0300

    add break

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 const contacts = require("./contacts");
 const { program } = require("commander");
 
+const fs = require('fs');
+
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
       return console.table(allContacts);
-      
+      break;
 
     case "get":
       const contactById = await contacts.getContactById(id);
       return console.log(contactById);
-      
+      break;
 
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
       return console.log(newContact);
-      
+      break;
 
     case "remove":
       const deleteContact = await contacts.removeContact(id);
       return console.log(deleteContact);
-      
+      break;
 
     default:
       return console.warn("\x1B[31m Unknown action type!");
